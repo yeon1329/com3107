7369c313a83983ed424b1d97521cec372e58d920
yeon1329 <kdy01037211329@gmail.com>
2024-10-18 10:43:08 +0900

    Add files via upload
    Add game loop

--- snakeGame.py
+++ snakeGame.py
@@ -3,6 +3,12 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((400,200))
+pygame.display.set_caption("Snake Game ver 1.0")
 pygame.display.update()
 
-pygame.quit()
+running = True
+while running:
+    for event in pygame.event.get():
+        print(event)
+
+#pygame.quit()
