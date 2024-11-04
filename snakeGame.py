d2a74100a9c69b71f5acae716bc51b4ea6546dc2
yeon1329 <kdy01037211329@gmail.com>
2024-11-04 11:11:04 +0900

    Update snakeGame.py

--- snakeGame.py
+++ snakeGame.py
@@ -1,18 +1,7 @@
 import pygame
-import time
 
-def message(fonts, msg, color, posx, posy):
-    mesg = fonts.render(msg, True, color)
-    mesg_Rect = mesg.get_rect()
-    mesg_Rect.centerx = posx
-    mesg_Rect.centery = posy
-    screen.blit(mesg, mesg_Rect)
 
 pygame.init()
-clock = pygame.time.Clock()
-#Fonts
-font_gameOver = pygame.font.SysFont(None, 50)
-font_madeBy = pygame.font.SysFont(None, 20)
 
 #color
 RED = (255,0,0); GREEN = (0,255,0)
@@ -23,12 +12,8 @@ SCR_WIDTH, SCR_HEIGHT = 800,600
 
 #snake
 SNAKE_SIZE = 20
-snake_pos_x  = SCR_WIDTH/2 - SNAKE_SIZE/2
-snake_pos_y  = SCR_HEIGHT/2 - SNAKE_SIZE/2
-snake_posx_change = 0
-snake_posy_change = 0
 
-screen = pygame.display.set_mode((SCR_WIDTH, SCR_HEIGHT))
+screen = pygame.display.set_mode((400,200))
 pygame.display.set_caption("Snake Game ver 1.0")
 
 
@@ -45,21 +30,7 @@ while running:
             if event.key == pygame.K_DOWN:
                 snake_posx_change = 0
                 snake_posy_change = 10
-            if event.key == pygame.K_LEFT:
-                snake_posx_change = -10
-                snake_posy_change = 0
-            if event.key == pygame.K_RIGHT:
-                snake_posx_change = 10
-                snake_posy_change = 0
-    if snake_pos_x >= (SCR_WIDTH - SNAKE_SIZE) or \
-       snake_pos_x - (SNAKE_SIZE/2) < 0 or \
-       snake_pos_y >= (SCR_HEIGHT - SNAKE_SIZE) or \
-       snake_pos_y - (SNAKE_SIZE/2) < 0 :
-        running = False       
-                
-    snake_pos_x += snake_posx_change
-    snake_pos_y += snake_posy_change
-    screen.fill(WHITE)
+            
 
     pygame.draw.rect(screen, BLACK, [0,0, SCR_WIDTH, SCR_HEIGHT],10)
     pygame.draw.rect(screen, RED, [snake_pos_x,
@@ -69,9 +40,5 @@ while running:
 
     
     pygame.display.update()
-    clock.tick(60)
-message(font_gameOver, 'Game Over', RED, int(SCR_WIDTH/2), int(SCR_HEIGHT/2))
-message(font_madeBy, 'made by yeon1329', BLACK, int(SCR_WIDTH/2), int(SCR_HEIGHT/2)+30)
-pygame.display.update()
-time.sleep(2)
+
 pygame.quit()
